f5e02aac66bc5778f5f0f21b482f8df426b8755c
Baki Tuncer <tuncerbaki@gmail.com>
2022-09-24 14:44:18 +0100

    baki

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+
+var sheep_count, other_count;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+sheep_count = 0;
+
+other_count = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  if (getNumberOrString(document.getElementById('text').value) == 'good') {
+    let element_sheep_count = document.getElementById('good_count');
+    element_sheep_count.innerText = sheep_count;
+    sheep_count = (typeof sheep_count === 'number' ? sheep_count : 0) + 1;
+  } else {
+    let element_other_count = document.getElementById('bad_count');
+    element_other_count.innerText = other_count;
+    other_count = (typeof other_count === 'number' ? other_count : 0) + 1;
+  }
+
+});
